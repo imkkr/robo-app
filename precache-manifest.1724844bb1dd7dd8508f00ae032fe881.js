755997dbfa6fc9303262e6396cab84e7653573fc
imkkr <54110021+imkkr@users.noreply.github.com>
2020-07-19 22:40:47 +0530

    Updates

---
+++ precache-manifest.1724844bb1dd7dd8508f00ae032fe881.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8d5e8c7ed12c1d529f577efe7bda542",
+    "url": "/robo-app/index.html"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/robo-app/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "bf9433a846e529d7f8a1",
+    "url": "/robo-app/static/css/main.5e1b6667.chunk.css"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/robo-app/static/js/2.e7e24cc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robo-app/static/js/2.e7e24cc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf9433a846e529d7f8a1",
+    "url": "/robo-app/static/js/main.3c9814d2.chunk.js"
+  },
+  {
+    "revision": "acc4aa42bc7fd7b2ec0c",
+    "url": "/robo-app/static/js/runtime-main.abd4af74.js"
+  },
+  {
+    "revision": "4b641e176a79681f76ef5fa739d0e18b",
+    "url": "/robo-app/static/media/Resotedream-JRaEM.4b641e17.ttf"
+  }
+]);
